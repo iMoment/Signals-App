39cf8ef4afa6fb5cf3642085044a1969692933c5
Stanley Pan <stanleypan.egre@gmail.com>
2016-08-14 19:54:15 +0900

    Create userInputContainerView and set up constraints

--- Signals/LoginController.swift
+++ Signals/LoginController.swift
@@ -9,11 +9,30 @@
 import UIKit
 
 class LoginController: UIViewController {
+    
+    let userInputContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.whiteColor()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        
+        view.addSubview(userInputContainerView)
+        
+        setupUserInputContainerView()
+    }
+    
+    func setupUserInputContainerView() {
+        // MARK: Constraints of x, y, width, height
+        userInputContainerView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        userInputContainerView.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor).active = true
+        userInputContainerView.widthAnchor.constraintEqualToAnchor(view.widthAnchor, constant: -24).active = true
+        userInputContainerView.heightAnchor.constraintEqualToConstant(150).active = true
     }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
@@ -21,6 +40,9 @@ class LoginController: UIViewController {
     }
     
 }
+
+
+
 
 extension UIColor {
     
