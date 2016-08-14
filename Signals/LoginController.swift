47b3bb3a4f4761efee390cc65e61ddad29e1a578
Stanley Pan <stanleypan.egre@gmail.com>
2016-08-14 19:37:12 +0900

    Create LoginViewController

---
+++ Signals/LoginController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginController.swift
+//  Signals
+//
+//  Created by Stanley Pan on 8/14/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
+}
+
+extension UIColor {
+    
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
+}
--- Signals/ViewController.swift
+++ Signals/ViewController.swift
@@ -11,6 +11,13 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: #selector(handleLogout))
+    }
+    
+    func handleLogout() {
+        let loginController = LoginController()
+        presentViewController(loginController, animated: true, completion: nil)
     }
 }
 
