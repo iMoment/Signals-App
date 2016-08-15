902c1569063f6720770b14c86ba664b6628f2317
Stanley Pan <stanleypan.egre@gmail.com>
2016-08-15 18:46:23 +0900

    Grab name using fetch for navigationItem title

--- Signals/MessagesController.swift
+++ Signals/MessagesController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MessagesController.swift
 //  Signals
 //
 //  Created by Stanley Pan on 8/14/16.
@@ -8,16 +8,30 @@
 import UIKit
 import Firebase
 
-class ViewController: UITableViewController {
-
+class MessagesController: UITableViewController {
+    // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: #selector(handleLogout))
         
         // User is not logged in
+        checkIfUserIsLoggedIn()
+    }
+    
+    func checkIfUserIsLoggedIn() {
         if FIRAuth.auth()?.currentUser?.uid == nil {
             performSelector(#selector(handleLogout), withObject: nil, afterDelay: 0)
+        } else {
+            let uid = FIRAuth.auth()?.currentUser?.uid
+            FIRDatabase.database().reference().child("users").child(uid!).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+                
+                print(snapshot)
+                if let dictionary = snapshot.value as? [String: AnyObject] {
+                    self.navigationItem.title = dictionary["name"] as? String
+                }
+                
+                }, withCancelBlock: nil)
         }
     }
     
@@ -32,4 +46,3 @@ class ViewController: UITableViewController {
         presentViewController(loginController, animated: true, completion: nil)
     }
 }
-
