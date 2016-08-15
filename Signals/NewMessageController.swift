1f7a4225e9cc0fde2e17a7836e5abeeb30784978
Stanley Pan <stanleypan.egre@gmail.com>
2016-08-15 19:26:31 +0900

    Work on tableView and fetchUser

--- Signals/NewMessageController.swift
+++ Signals/NewMessageController.swift
@@ -6,11 +6,42 @@
 //  Copyright © 2016 Stanley Pan. All rights reserved.
 
 import UIKit
+import Firebase
 
 class NewMessageController: UITableViewController {
-
+    // MARK: Properties
+    let cellId = "cellId"
+    
+    // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action: #selector(handleCancel))
+        
+        fetchUser()
+    }
+    
+    func fetchUser() {
+        FIRDatabase.database().reference().child("users").observeEventType(.ChildAdded, withBlock: { (snapshot) in
+            
+            print(snapshot)
+            
+            }, withCancelBlock: nil)
+    }
+    
+    func handleCancel() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellId)
+        
+        cell.textLabel?.text = "Placeholder Text For Now"
+        
+        return cell
     }
 }
